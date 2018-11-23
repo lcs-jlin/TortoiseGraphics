910e585ee067b73d5f060c48f9405a15615b1a91
Justy Lin <jlin@lcs.on.ca>
2018-11-23 10:39:49 -0500

    Art Line 1

--- Playground/Playground.playground/Pages/Art Line 2.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Art Line 2.xcplaygroundpage/Contents.swift
@@ -1,36 +1,7 @@
-import PlaygroundSupport
-import TortoiseGraphics
-import CoreGraphics
+//: [Previous](@previous)
 
-let canvas = PlaygroundCanvas(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
-canvas.frameRate = 300
-canvas.color = .white
-PlaygroundPage.current.liveView = canvas
+import Foundation
 
-canvas.drawing { turtle in
-    turtle.penColor(.blueGrey)
-    for _ in 1...9 {
-        turtle.right(40)
-    for _ in 1...9 {
-            turtle.right(40)
-            turtle.forward(40)
-        }
-    }
-    for _ in 1...9{
-        turtle.right(40)
-    for _ in 1...9{
-            turtle.right(40)
-            turtle.forward(80)
-        }
-    }
-    for _ in 1...2{
-    for _ in 1...3{
-        turtle.right(60)
-        for _ in 1...3 {
-            turtle.right(120)
-            turtle.forward(290)
-        }
-    }
-    }
-    turtle.hideTortoise()
-}
+var str = "Hello, playground"
+
+//: [Next](@next)
